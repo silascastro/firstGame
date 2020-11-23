27afea1c77974be2a660d1ff0e18198116e42d16
Silas Castro <silas.castro@unasusamazonia.com.br>
2020-11-23 15:36:47 -0400

    first commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMoviment moviment;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.collider.tag == "obstacle")
+        {
+            moviment.enabled = false;
+        }
+
+    }
+
+}
